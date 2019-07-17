2b2d68053f6b5909ead9a233255fc5599fbb35b0
Yuma <yuma.nihei@tier4.jp>
2019-07-17 16:25:57 +0900

    except non numeric string

--- bin/net_monitor.py
+++ bin/net_monitor.py
@@ -60,6 +60,13 @@ net_capacity = 128
 
 stat_dict = {0: 'OK', 1: 'Warning', 2: 'Error'}
 
+def delete_non_numeric_string(str):
+  try:
+    target = string(float(str))
+  except:
+    target = "0.0"
+  return (target)
+
 def update_status_stale(stat, last_update_time):
   time_since_update = rospy.get_time() - last_update_time
   stale_status = 'OK'
@@ -158,11 +165,11 @@ class NetMonitor():
           if ifacematch and (cmd_out == 'down' or cmd_out == 'dormant'):
             level = DiagnosticStatus.ERROR
         values.append(KeyValue(key = 'Input Traffic',
-          value = str(float(kb_in[i]) / 1024) + " (MB/s)"))
+          value = str(float(delete_non_numeric_string(kb_in[i])) / 1024) + " (MB/s)"))
         values.append(KeyValue(key = 'Output Traffic',
-          value = str(float(kb_out[i]) / 1024) + " (MB/s)"))
-        net_usage_in = float(kb_in[i]) / 1024 / self._net_capacity
-        net_usage_out = float(kb_out[i]) / 1024 / self._net_capacity
+          value = str(float(delete_non_numeric_string(kb_out[i])) / 1024) + " (MB/s)"))
+        net_usage_in = float(delete_non_numeric_string(kb_in[i])) / 1024 / self._net_capacity
+        net_usage_out = float(delete_non_numeric_string(kb_out[i])) / 1024 / self._net_capacity
         if net_usage_in > self._net_level_warn or\
           net_usage_out > self._net_level_warn:
           level = DiagnosticStatus.WARN
@@ -172,11 +179,11 @@ class NetMonitor():
         (retcode, cmd_out) = get_sys_net_stat(ifaces[i], 'rx_bytes')
         if retcode == 0:
           values.append(KeyValue(key = 'Total received MB',
-            value = str(float(cmd_out) / 1024 / 1024)))
+            value = str(float(delete_non_numeric_string(cmd_out)) / 1024 / 1024)))
         (retcode, cmd_out) = get_sys_net_stat(ifaces[i], 'tx_bytes')
         if retcode == 0:
           values.append(KeyValue(key = 'Total transmitted MB',
-            value = str(float(cmd_out) / 1024 / 1024)))
+            value = str(float(delete_non_numeric_string(cmd_out)) / 1024 / 1024)))
         (retcode, cmd_out) = get_sys_net_stat(ifaces[i], 'collisions')
         if retcode == 0:
           values.append(KeyValue(key = 'Collisions', value = cmd_out))
